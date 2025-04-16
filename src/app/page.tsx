79430238357930b97bca69a3bf54aef3e0c4f037
Luiz Vinicius <viniciusadr244@gmail.com>
2025-04-16 14:54:59 -0300

    feat: enhance login form UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "@/components/LoginForm";
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Faça login na sua conta</h1>
+		<div className="min-h-screen flex flex-col items-center justify-center">
+			<h1 className="text-2xl font-bold">Faça login na sua conta</h1>
 			<LoginForm />
 		</div>
 	);
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, EyeOff, Lock, Mail } from "lucide-react";
 import { useState } from "react";
 
 const loginFormSchema = z
@@ -44,71 +44,86 @@ export function LoginForm() {
 		input: "password",
 		visible: false,
 	});
-	console.log(passwordVisible);
 
 	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)}>
-				<FormField
-					control={form.control}
-					name="email"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Email</FormLabel>
-							<FormControl>
-								<Input type="email" placeholder="Email" {...field} />
-							</FormControl>
-							<span style={{ color: "red" }}>
-								{form.formState?.errors?.email?.message}
-							</span>
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name="password"
-					render={({ field }) => (
-						<FormItem>
-							<FormLabel>Senha</FormLabel>
-							<FormControl>
+		<div className="flex flex-col gap-y-2 w-100 h-64">
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col h-full">
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className="text-xl">Email</FormLabel>
 								<div className="relative">
-									<Input
-										type={passwordVisible.input}
-										placeholder="Senha"
-										{...field}
-									/>
-									<Eye
-										className={`absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer ${
-											passwordVisible.visible ? "block" : "hidden"
-										}`}
-										onClick={() =>
-											setPasswordVisible({
-												input: "password",
-												visible: false,
-											})
-										}
-									/>
-									<EyeOff
-										className={`absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer ${
-											passwordVisible.visible ? "hidden" : "block"
-										}`}
-										onClick={() =>
-											setPasswordVisible({
-												input: "text",
-												visible: true,
-											})
-										}
-									/>
+									<Mail className="absolute left-1 top-1/2 transform -translate-y-1/2" />
+									<FormControl>
+										<Input
+											type="email"
+											placeholder="Email"
+											className="pl-8"
+											{...field}
+										/>
+									</FormControl>
 								</div>
-							</FormControl>
-							<span style={{ color: "red" }}>
-								{form.formState?.errors?.password?.message}
-							</span>
-						</FormItem>
-					)}
-				/>
-				<Button type="submit">Login</Button>
-			</form>
-		</Form>
+								<span className="text-sm text-red-500">
+									{form.formState?.errors?.email?.message}
+								</span>
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className="text-xl">Senha</FormLabel>
+								<FormControl>
+									<div className="relative">
+										<Lock className="absolute left-1 top-1/2 transform -translate-y-1/2" />
+										<Input
+											type={passwordVisible.input}
+											placeholder="Senha"
+											className="pl-8"
+											{...field}
+										/>
+										<Eye
+											className={`absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer ${
+												passwordVisible.visible ? "block" : "hidden"
+											}`}
+											onClick={() =>
+												setPasswordVisible({
+													input: "password",
+													visible: false,
+												})
+											}
+										/>
+										<EyeOff
+											className={`absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer ${
+												passwordVisible.visible ? "hidden" : "block"
+											}`}
+											onClick={() =>
+												setPasswordVisible({
+													input: "text",
+													visible: true,
+												})
+											}
+										/>
+									</div>
+								</FormControl>
+								<span className="text-sm text-red-500">
+									{form.formState?.errors?.password?.message}
+								</span>
+							</FormItem>
+						)}
+					/>
+					<div className="flex justify-center mt-auto">
+						<Button type="submit" className="w-[90%]">
+							Login
+						</Button>
+					</div>
+				</form>
+			</Form>
+		</div>
 	);
 }
